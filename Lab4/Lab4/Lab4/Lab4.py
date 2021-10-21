3c4d49c860ec6e1a0296362ce5649d86744f98c1
rberezov <rodion.berezov.2001@mail.ru>
2021-10-21 23:40:34 +0300

    Update PEP8

--- Lab4/Lab4/Lab4/Lab4.py
+++ Lab4/Lab4/Lab4/Lab4.py
@@ -3,7 +3,6 @@ import json
 from math import sqrt
 from random import randint
 import pygame
-from pygame.draw import *
 pygame.init()
 
 FPS = 90
@@ -24,8 +23,6 @@ class Ball:
     """ A ball is being created
     number_of_clicks_to_delete - Number of taps to remove the ball
     lives - Number of lives
-    x, y, r  - Coordinates of the center of the ball and radius
-    v - Speed
     """
     max_radius = 99
     min_radius = 30
@@ -55,7 +52,7 @@ class Ball:
         """
         A ball is being drawn
         """
-        circle(screen, self.color, (self.x_coordinate, self.y_coordinate), self.radius)
+        pygame.draw.circle(screen, self.color, (self.x_coordinate, self.y_coordinate), self.radius)
 
     def delete(self):
         """
@@ -100,7 +97,7 @@ def number(num):
                     tempx = tempx + x_coordinate
                     tempy = tempy - y_coordinate
                 else:
-                    line(screen, (255, 255, 255), (tempx, tempy),
+                    pygame.draw.line(screen, (255, 255, 255), (tempx, tempy),
                         (posx + x_coordinate, posy - y_coordinate), 10)
                     tempx = posx + x_coordinate
                     tempy = posy - y_coordinate
@@ -113,8 +110,8 @@ with open ('input.json', 'rt') as f:
 
 FINISHED = False
 NICKNAME = ''
-number_of_symbol = 0
-number_of_enter = 0
+NUMBER_OF_SYMBOL = 0
+NUMBER_OF_ENTER = 0
 while not FINISHED: #Entering the user name
     f2 = pygame.font.Font(None, 50)
     text2 = f2.render('Plese, enter your nickname:', True, (180, 0, 0))
@@ -127,52 +124,53 @@ while not FINISHED: #Entering the user name
             if pygame.key.name(event.key) == 'escape':
                 FINISHED = True
             elif pygame.key.name(event.key) == 'return':
-                if number_of_enter == 0:
+                if NUMBER_OF_ENTER == 0:
                     if data.get(NICKNAME) is None:
                         data[NICKNAME] = 0
                     text3 = f2.render('Your record:' + str(data[NICKNAME]),
                                      True, (180, 0, 0))
                     screen.blit(text3, (400, 500))
-                    number_of_enter = number_of_enter + 1
+                    NUMBER_OF_ENTER = NUMBER_OF_ENTER + 1
                 else:
-                    number_of_enter = number_of_enter + 1
+                    NUMBER_OF_ENTER = NUMBER_OF_ENTER + 1
                     FINISHED = True
             elif pygame.key.name(event.key) == 'space':
-                number_of_symbol = number_of_symbol + 1
+                NUMBER_OF_SYMBOL = NUMBER_OF_SYMBOL + 1
                 NICKNAME = NICKNAME + ' '
             elif pygame.key.name(event.key) == 'left shift' \
                or pygame.key.name(event.key) == 'right shift':
                 NICKNAME = NICKNAME + ''
             elif pygame.key.name(event.key) == 'backspace':
-                if number_of_symbol != 0 and number_of_enter == 0:
+                if NUMBER_OF_SYMBOL != 0 and NUMBER_OF_ENTER == 0:
                     NICKNAME = NICKNAME[:-1]
-                    number_of_symbol = number_of_symbol - 1
-                    rect(screen, (0, 0, 0), (430 + number_of_symbol * 30, 450, 50, 50))
+                    NUMBER_OF_SYMBOL = NUMBER_OF_SYMBOL - 1
+                    pygame.draw.rect(screen, (0, 0, 0),
+                                    (430 + NUMBER_OF_SYMBOL * 30, 450, 50, 50))
             else:
                 keyname = pygame.key.name(event.key)
                 NICKNAME = NICKNAME + keyname
                 f1 = pygame.font.Font(None, 50)
-                number_of_symbol = number_of_symbol + 1
+                NUMBER_OF_SYMBOL = NUMBER_OF_SYMBOL + 1
                 text1 = f1.render(keyname, True, (180, 0, 0))
-                screen.blit(text1, (400 + number_of_symbol * 30, 450))
-if number_of_enter == 2:
+                screen.blit(text1, (400 + NUMBER_OF_SYMBOL * 30, 450))
+if NUMBER_OF_ENTER == 2: #Checks whether the player has exited
     FINISHED = False
 
-list_of_balls = []
-score = 0
-number_of_while = 0
+LIST_OF_BALLS = []
+SCORE = 0
+NUMBER_OF_EXECUTIONS_MAIN_CYCLES = 0
 
 while not FINISHED: #The main cycle of the game
     clock.tick(FPS)
-    number_of_while = number_of_while + 1
+    NUMBER_OF_EXECUTIONS_MAIN_CYCLES = NUMBER_OF_EXECUTIONS_MAIN_CYCLES + 1
 
-    if number_of_while % 30 == 0 or number_of_while == 0:
-        list_of_balls.append(Ball())
-    for i in range(len(list_of_balls)):
-        list_of_balls[i].moving()
-        list_of_balls[i].draw()
+    if NUMBER_OF_EXECUTIONS_MAIN_CYCLES % 30 == 0 or NUMBER_OF_EXECUTIONS_MAIN_CYCLES == 0:
+        LIST_OF_BALLS.append(Ball())
+    for i in range(len(LIST_OF_BALLS)):
+        LIST_OF_BALLS[i].moving()
+        LIST_OF_BALLS[i].draw()
 
-    number(score)
+    number(SCORE)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -181,52 +179,52 @@ while not FINISHED: #The main cycle of the game
             if pygame.key.name(event.key) == 'escape':
                 FINISHED = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            the_number_of_balls_hit = 0
-            for i in reversed(range(len(list_of_balls))):
-                if (event.pos[0] - list_of_balls[i].x_coordinate) ** 2 \
-                    + (event.pos[1] - list_of_balls[i].y_coordinate) ** 2 \
-                    < list_of_balls[i].radius ** 2:
-                    the_number_of_balls_hit = the_number_of_balls_hit + 1
-                    if list_of_balls[i].number_of_clicks_to_delete - 1 > 0:
-                        list_of_balls[i].number_of_clicks_to_delete = \
-                            list_of_balls[i].number_of_clicks_to_delete - 1
+            THE_NUMBER_OF_BALLS_HIT = 0
+            for i in reversed(range(len(LIST_OF_BALLS))):
+                if (event.pos[0] - LIST_OF_BALLS[i].x_coordinate) ** 2 \
+                    + (event.pos[1] - LIST_OF_BALLS[i].y_coordinate) ** 2 \
+                    < LIST_OF_BALLS[i].radius ** 2:
+                    THE_NUMBER_OF_BALLS_HIT = THE_NUMBER_OF_BALLS_HIT + 1
+                    if LIST_OF_BALLS[i].number_of_clicks_to_delete - 1 > 0:
+                        LIST_OF_BALLS[i].number_of_clicks_to_delete = \
+                            LIST_OF_BALLS[i].number_of_clicks_to_delete - 1
                     else:
-                        list_of_balls[i].delete()
-                        score = score + list_of_balls[i].max_radius \
-                            // list_of_balls[i].radius \
-                            + round(sqrt(list_of_balls[i].velocity[1] ** 2 \
-                           + list_of_balls[i].velocity[1] ** 2)) // 3 \
-                           + list_of_balls[i].number_of_clicks_to_delete // 2
-                        list_of_balls.pop(i)
-            if the_number_of_balls_hit == 0 and score != 0:
-                score = score - 1
+                        LIST_OF_BALLS[i].delete()
+                        SCORE = SCORE + LIST_OF_BALLS[i].max_radius \
+                            // LIST_OF_BALLS[i].radius \
+                            + round(sqrt(LIST_OF_BALLS[i].velocity[1] ** 2 \
+                           + LIST_OF_BALLS[i].velocity[1] ** 2)) // 3 \
+                           + LIST_OF_BALLS[i].number_of_clicks_to_delete // 2
+                        LIST_OF_BALLS.pop(i)
+            if THE_NUMBER_OF_BALLS_HIT == 0 and SCORE != 0:
+                SCORE = SCORE - 1
 
 
-    for i in reversed(range(len(list_of_balls))):
-        if list_of_balls[i].lives - 1 >= 0:
-            list_of_balls[i].lives = list_of_balls[i].lives - 1
+    for i in reversed(range(len(LIST_OF_BALLS))):
+        if LIST_OF_BALLS[i].lives - 1 >= 0:
+            LIST_OF_BALLS[i].lives = LIST_OF_BALLS[i].lives - 1
         else:
-            list_of_balls[i].delete()
-            list_of_balls.pop(i)
+            LIST_OF_BALLS[i].delete()
+            LIST_OF_BALLS.pop(i)
 
     screen.fill(BLACK)
 
-if score > data[NICKNAME]: #Congratulations to the player if he make the record
+if SCORE > data[NICKNAME]: #Congratulations to the player if he make the record
     screen.fill(BLACK)
-    data[NICKNAME] = score
+    data[NICKNAME] = SCORE
     with open ('input.json', 'wt') as f:
         json.dump(data, f)
-    finished = False
-    while not finished:
+    FINISHED = False
+    while not FINISHED:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                finished = True
+                FINISHED = True
             elif event.type == pygame.KEYDOWN:
                 if pygame.key.name(event.key) == 'escape':
                     FINISHED = True
         f4 = pygame.font.Font(None, 50)
-        text4 = f4.render('Congratulations! You have a new record:' + ' ' +str(score),
-                         True, (180, 0, 0))
+        text4 = f4.render('Congratulations! You have a new record:'\
+           + ' ' + str(SCORE), True, (180, 0, 0))
         screen.blit(text4, (350 , 400))
         pygame.display.update()
 pygame.quit()
